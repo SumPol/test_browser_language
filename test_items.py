cbce2e86876164ee12d31cc116d21ead56b535d7
Polina <polya_star95@mail.ru>
2021-02-17 00:09:57 +0300

    added files

---
+++ test_items.py
@@ -0,0 +1,8 @@
+link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+import time
+
+def test_add_to_basket_button_exist(browser):
+    browser.get(link)
+    time.sleep(30)
+    btn = browser.find_elements_by_css_selector("#add_to_basket_form>button")
+    assert len(btn)>0 , 'There is no "Add to basket" button'
